ef94d3a415f821dc9b415e952ba2b48face9b421
Sohini Pattanayak <sohinipattanayak.it@gmail.com>
2021-06-17 15:31:48 +0000

    cleaned code

--- index.js
+++ index.js
@@ -3,15 +3,41 @@ const client = new Discord.Client();
 
 const mySecret = process.env['DISCORD_TOKEN']
 
+var Airtable = require('airtable');
+var base = new Airtable({ apiKey: 'API_KEY_HERE' }).base('BASE_ID');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    client.channels.chache.get(`855009473394638889`).send('Pong!');
+client.on('message', async message => {
+  if (message.author === client.user) {
+    return;
+  }
+
+  try {
+    base('Information').create([
+  {
+    "fields": {
+      "UName": `${message.author.username}`,
+      "Feedback": `${message.content}`
+    }
+  }
+], function(err, records) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  records.forEach(function (record) {
+    console.log(record.getId());
+  });
+});
+    client.channels.cache.get(`CHANNEL_ID`).send('Thank you for your feedback!');
+  }
+  catch (err) {
+    console.log(err);
   }
 });
 
 client.login(mySecret);
+
